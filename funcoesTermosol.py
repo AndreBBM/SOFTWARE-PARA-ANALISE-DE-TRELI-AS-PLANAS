44a1b08d0b5595295238b8c3bdde745bc4e01df0
AndreBBM <andrebbmelo@hotmail.com>
2023-05-25 01:39:18 -0300

    calculando deslocamento nodal e deformação especifica de cada elemento

--- funcoesTermosol.py
+++ funcoesTermosol.py
@@ -169,7 +169,23 @@ def geraSaida(nome,Ft,Ut,Epsi,Fi,Ti):
     f.write(str(Ti))
     f.close()
 
+def gauss_seidel(A, b, x0, epsilon, max_iterations):
+    n = len(A)
+    x = x0.copy()
 
-# [nn,N,nm,Inc,nc,F,nr,R, L, angle] = importa('entrada.xlsx')
+    #Gauss-Seidal Method [By Bottom Science]
+
+    for i in range(max_iterations):
+        x_new = np.zeros(n)
+        for j in range(n):
+            s1 = np.dot(A[j, :j], x_new[:j])
+            s2 = np.dot(A[j, j + 1:], x[j + 1:])
+            x_new[j] = (b[j] - s1 - s2) / A[j, j]
+        if np.allclose(x, x_new, rtol=epsilon):
+            return x_new
+        x = x_new
+    return x
+
+[nn,N,nm,Inc,nc,F,nr,R, L, angle] = importa('entrada.xlsx')
 # plota(N,Inc)
 # geraSaida('teste',F,F,F,F,F)
